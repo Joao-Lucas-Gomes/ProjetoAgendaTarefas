ef5c7178a66212ca60fb89bdac02cdb55f1c2674
Joao-Lucas-Gomes <joaodejesuita@gmail.com>
2025-11-05 21:43:32 -0300

    código consertado. iniciar projeto AgendaTarefas

--- src/main/java/com/senac/aulaapijovemprogramador/application/dto/usuario/UsuarioCriarRequestDto.java
+++ src/main/java/com/senac/aulaapijovemprogramador/application/dto/usuario/UsuarioCriarRequestDto.java
@@ -1,5 +1,6 @@
 package com.senac.aulaapijovemprogramador.application.dto.usuario;
 
+
 import java.util.List;
 
 public record UsuarioCriarRequestDto (String nome, String email, String senha, String cpf, boolean isAdm){
